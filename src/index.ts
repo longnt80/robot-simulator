e69c1aaef4801a84b5db736caa2ac4184c79cea0
Long Nguyen <longnt80@gmail.com>
2024-05-19 09:28:14 +1000

    Update index - To process user input to un-sanitised string for Simulator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
 import { Simulator } from "./Simulator";
+import * as readline from "readline";
 
-const simulator = new Simulator().simulate(process.argv.slice(2));
+function main() {
+  const simulator = new Simulator();
+
+  let commandString: string = "";
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.setPrompt(
+    ["Enter your commands", "Press Ctrl+C to simulate the robot", "\n"].join(
+      "\n"
+    )
+  );
+  rl.prompt();
+
+  rl.on("line", (input) => {
+    commandString = commandString + " " + input;
+  });
+
+  rl.on("SIGINT", () => {
+    rl.question("Do you want to simulate the robot now? (y/N): ", (answer) => {
+      if (answer.match(/^y(es)?$/i)) {
+        simulator.simulate(commandString);
+        rl.close();
+      }
+    });
+  });
+}
+
+main();
