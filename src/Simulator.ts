11448e4a4c100f9fd629e11a65bb4f096dec4ef9
Long Nguyen <longnt80@gmail.com>
2024-05-19 09:28:42 +1000

    Update Simulator class

--- src/Simulator.ts
+++ src/Simulator.ts
@@ -2,11 +2,11 @@ import { Directions } from "./Directions";
 import { Robot } from "./Robot";
 
 export class Simulator {
-  private parser(command: string[]) {
-    console.log(command);
+  private parser(command: string) {
+    console.log({ command });
   }
 
-  public simulate(command: string[]) {
+  public simulate(command: string) {
     const validCommands = this.parser(command);
     const directions = new Directions();
     const robot = new Robot(directions);
